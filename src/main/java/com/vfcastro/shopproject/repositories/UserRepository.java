33b105acc2fa25ba300be0c255cd8f98bd7e8b0e
Castruu <vitorfycastro@hotmail.com>
2022-05-16 14:57:23 -0300

    jpa repository, dependency injection, database seeding

---
+++ src/main/java/com/vfcastro/shopproject/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vfcastro.shopproject.repositories;
+
+
+import com.vfcastro.shopproject.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
